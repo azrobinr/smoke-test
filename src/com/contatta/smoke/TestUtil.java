68b04fbc89190dbe012efed132192cff89bfec1e
Robin Robbins <robin@contatta.com>
2013-06-18 16:04:07 -0700

    first commit of the first class in smoke-test, and we're starting with the TestUtil

---
+++ src/com/contatta/smoke/TestUtil.java
@@ -0,0 +1,225 @@
+package com.contatta.smoke;
+
+import java.util.Iterator;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.apache.log4j.Logger;
+
+public class TestUtil {
+	private static Logger log4j = Logger.getLogger(TestUtil.class.getName());
+	private static WebDriver driver ;
+	private static WebElement element = null;
+	private static String pageTitle = "";
+	private static int z = 5;
+	
+	TestUtil(WebDriver drv, Logger logger, int snoozeValue){
+		log4j = logger;
+		driver = drv;
+		z = snoozeValue;
+		
+	}
+		
+	void clear(String selector) {
+		log4j.info("clicking " + selector);
+		element = null;
+		try {
+			element = driver.findElement(By.cssSelector(selector));
+		}
+		catch (NoSuchElementException e) {
+			log4j.error("didn't find " + selector);
+			driver.quit();	
+			System.exit(1);
+		}
+		catch(ElementNotVisibleException e){
+    		log4j.error(selector + " is not Visible?");
+    		log4j.error("...this exception was caught in TestUtil.");
+    		driver.close();
+    		System.exit(1);
+    	}
+		catch(Exception e){
+			String line = "Some other kind of exception " 
+					+ e.toString()
+					+ "\n";
+			
+			log4j.error(line);
+			driver.quit();	
+			System.exit(1);
+        }
+		element.clear();
+		this.snooze(z);
+		
+	}
+	
+	void click(String selector) {
+		log4j.info("clicking " + selector);
+		element = null;
+		try {
+			element = driver.findElement(By.cssSelector(selector));
+		}
+		catch (NoSuchElementException e) {
+			log4j.error("didn't find " + selector);
+			driver.quit();	
+			System.exit(1);
+		}
+		catch(ElementNotVisibleException e){
+    		log4j.error(selector + " is not Visible?");
+    		log4j.error("...this exception was caught in TestUtil.");
+    		driver.close();
+    		System.exit(1);
+    	}
+		catch(Exception e){
+			String line = "Some other kind of exception " 
+					+ e.toString()
+					+ "\n";
+			
+			log4j.error(line);
+			driver.quit();	
+			System.exit(1);
+        }
+		element.click();
+		this.snooze(z);
+		
+	}
+
+	String getAttribute(String selector, String name){
+		log4j.info("clicking " + selector);
+		element = null;
+		try {
+			element = driver.findElement(By.cssSelector(selector));
+		}
+		catch (NoSuchElementException e) {
+			log4j.error("didn't find " + selector);
+			driver.quit();	
+			System.exit(1);
+		}
+		catch(ElementNotVisibleException e){
+    		log4j.error(selector + " is not Visible?");
+    		log4j.error("...this exception was caught in TestUtil.");
+    		driver.close();
+    		System.exit(1);
+    	}
+		catch(Exception e){
+			String line = "Some other kind of exception " 
+					+ e.toString()
+					+ "\n";
+			
+			log4j.error(line);
+			driver.quit();	
+			System.exit(1);
+        }
+		return (element.getAttribute(name));
+	}
+	
+	Iterator<WebElement> getGridIterator(String selector){
+		List<WebElement> elements = null;
+		Iterator<WebElement> iterator;
+		try{
+			elements = driver.findElements(By.cssSelector(selector));
+			int size = elements.size();
+			log4j.info("looks like there are " + size + " elements currently pulled in the dgrid");
+			Assert.assertTrue(size>0);
+			
+		}
+		catch(AssertionError e){
+			log4j.error("Warning: Expected grid to contain some elements, it didn't");
+			driver.quit();	
+			System.exit(1);
+		}
+		catch (NoSuchElementException e) {
+			log4j.error("didn't find " + selector);
+			driver.quit();	
+			System.exit(1);
+		}
+		catch(ElementNotVisibleException e){
+    		log4j.error(selector + " is not Visible?");
+    		log4j.error("...this exception was caught in TestUtil.");
+    		driver.close();
+    		System.exit(1);
+    	}
+		iterator = elements.iterator();
+		return iterator;
+	}
+	
+	String getText(String selector){
+		log4j.info("clicking " + selector);
+		element = null;
+		try {
+			element = driver.findElement(By.cssSelector(selector));
+		}
+		catch (NoSuchElementException e) {
+			log4j.error("didn't find " + selector);
+			driver.quit();	
+			System.exit(1);
+		}
+		catch(ElementNotVisibleException e){
+    		log4j.error(selector + " is not Visible?");
+    		log4j.error("...this exception was caught in TestUtil.");
+    		driver.close();
+    		System.exit(1);
+    	}
+		catch(Exception e){
+			String line = "Some other kind of exception " 
+					+ e.toString()
+					+ "\n";
+			
+			log4j.error(line);
+			driver.quit();	
+			System.exit(1);
+        }
+		return (element.getText());
+	}
+	
+	void send(String selector, String keys) {
+		log4j.info("sending " + keys + " to " + selector);
+		try {
+			element = driver.findElement(By.cssSelector(selector));
+			element.sendKeys(keys);
+		}
+		catch (NoSuchElementException e) {
+			log4j.warn("didn't find " + selector);
+			driver.quit();	
+			System.exit(1);
+		}
+		catch(ElementNotVisibleException e){
+    		log4j.error(selector + " is not Visible?");
+    		log4j.error("...this exception was caught in TestUtil.");
+    		driver.close();
+    		System.exit(1);
+    	}
+		catch(Exception e){
+			String line = "Some other kind of exception " 
+					+ e.toString()
+					+ "\n";
+			
+			log4j.error(line);
+			driver.quit();	
+			System.exit(1);
+        }
+		
+		this.snooze(z);
+	}
+	
+	void snooze(int n){
+		try{
+        	Thread.sleep(n*1000);
+        	}
+        catch(Exception e){
+        	log4j.error("sleep interrupted");
+			driver.quit();	
+			System.exit(1);
+        	}	
+	
+	}
+	
+	void whereAmI(){
+		pageTitle = driver.getTitle();
+		log4j.info("we are at " + pageTitle);
+	}
+
+
+}
